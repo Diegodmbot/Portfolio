f12c2ad84b64d919f123b709fd13d88d6a2eabf9
Diego Díaz Morón <alu0101337975@ull.edu.es>
2025-10-22 19:10:47 +0100

    feat: add download cv button functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { Curriculum } from "./Curriculum";
 import "./Header.css";
 import { ThemeToggle } from "./ThemeToggle";
+import ReactPDF from "@react-pdf/renderer";
 
 export function Header() {
+  const downloadCV = async () => {
+    const pdfBlob = await ReactPDF.pdf(<Curriculum />).toBlob();
+    const url = URL.createObjectURL(pdfBlob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "Diego_Díaz_Morón_CV.pdf";
+    a.click();
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <header>
       <nav className="HeaderNav">
@@ -11,11 +23,7 @@ export function Header() {
         <a className="HeaderLink" href="#projects">
           Projects
         </a>
-        <a
-          className="HeaderLink"
-          href="/Diego_Díaz_Morón_CV.pdf"
-          download={"Diego_Díaz_Morón_CV.pdf"}
-        >
+        <a className="HeaderLink" onClick={downloadCV}>
           Download CV
         </a>
         <ThemeToggle />
